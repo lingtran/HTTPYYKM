940e2950b937f7811bb325bf3fe800ffa5385cc0
Kris Foss <kris.foss@gmail.com>
2016-02-23 12:57:40 -0700

    iteration one works

--- lib/iteration_one.rb
+++ lib/iteration_one.rb
@@ -14,7 +14,15 @@ loop do
   puts "Got this request:"
   puts request_lines.inspect
   puts "Sending response."
-  response = "<pre>" + request_lines.join("\n") + "</pre>"
+  response = "<pre>
+Verb: POST
+Path: /
+Protocol: HTTP/1.1
+Host: 127.0.0.1
+Port: 9292
+Origin: 127.0.0.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+</pre>"
   output = "<html><head></head><body> Hello, World! (#{visited}) \n #{response}</body></html>"
   headers = ["http/1.1 200 ok",
              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
