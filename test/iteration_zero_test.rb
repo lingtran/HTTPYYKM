69c03f1faec73e13d23bee3407eb937a885741d8
lingtran <lxiutran@gmail.com>
2016-02-22 19:51:03 -0700

    refactored server and client sides

--- test/iteration_zero_test.rb
+++ test/iteration_zero_test.rb
@@ -1,7 +1,18 @@
 require '../lib/iteration_zero'
 require 'faraday'
 
+conn = Faraday.new(:url => 'http://127.0.0.1:9292/') do |faraday|
+  faraday.request  :url_encoded
+  faraday.response :logger
+  faraday.adapter  Faraday.default_adapter
+end
 
-response = Faraday.get 'http://127.0.0.1:9292'
+response = conn.get 'http://127.0.0.1:9292/'
+response.body
 
-puts response.body
+# conn.get
+#
+# conn.get do |req|
+#   req.url '/search', :page => 2
+#   req.params['limit'] = 100
+# end
