145467874d2a8052ae07fd5fd609d2c93638a9c6
Kris Foss <kris.foss@gmail.com>
2016-02-23 16:23:26 -0700

    methods built

---
+++ test/iteration_two_test.rb
@@ -0,0 +1,26 @@
+require 'faraday'
+require 'iteration_two'
+
+class IterationTwoTest < Minitest::TEST
+
+conn = Faraday.new(:url => 'http://127.0.0.1:9292') do |faraday|
+  faraday.request  :url_encoded
+  faraday.response :logger
+  faraday.adapter  Faraday.default_adapter
+end
+
+response = conn.get '/'
+puts response.body
+
+def test_it_can_get_a_request
+  assert Faraday.get 'http://127.0.0.1:9292', server.success?
+end
+
+def test_it_can_send_a_response
+end
+
+def test_it_can_prep_a_response
+end
+
+def test_it_can_send_a_response
+end
