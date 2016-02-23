4e392a38268c7a6142b8bc65f7728dc7056d3afe
lingtran <lxiutran@gmail.com>
2016-02-22 23:05:32 -0700

    added <pre>block</pre> to server file

--- lib/iteration_one.rb
+++ lib/iteration_one.rb
@@ -7,21 +7,25 @@ visited = 0
 loop do
   puts "Ready for a request!"
   request_lines = []
-
   while line = client.gets and !line.chomp.empty?
     request_lines << line.chomp
   end
-
   puts "Got this request:"
   puts request_lines.inspect
   visited += 1
   # Question - noticed that number goes up to 2, then goes back to one, even when we don't exit pry. Is that normal?
 
-
   puts "Sending response."
-  response = "<pre>" + "Hello World! (#{visited})" + "</pre>"
-
-  output = "<html><head></head><body>#{response}</body></html>"
+  response = "<pre>
+Verb: POST
+Path: /
+Protocol: HTTP/1.1
+Host: 127.0.0.1
+Port: 9292
+Origin: 127.0.0.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+</pre>"
+  output = "<html><head></head><body> Hello, World! #{visited} \n #{response}</body></html>"
   headers = ["http/1.1 200 ok",
              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
              "server: ruby",
@@ -30,5 +34,6 @@ loop do
   client.puts headers
   client.puts output
   puts ["Wrote this response:", headers, output].join("\n")
+  client.close
 end
-client.close
+puts "\nRepsonse complete, exiting."
