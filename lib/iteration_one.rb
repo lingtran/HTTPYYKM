3929554612a0ef0975fc89ce15542dbeff072f09
Kris Foss <kris.foss@gmail.com>
2016-02-23 12:49:25 -0700

    at lunch

--- lib/iteration_one.rb
+++ lib/iteration_one.rb
@@ -1,10 +1,11 @@
 require 'socket'
 
 tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
 
 visited = 0
 loop do
+  client = tcp_server.accept
+  visited += 1
   puts "Ready for a request!"
   request_lines = []
   while line = client.gets and !line.chomp.empty?
@@ -12,20 +13,9 @@ loop do
   end
   puts "Got this request:"
   puts request_lines.inspect
-  visited += 1
-  # Question - noticed that number goes up to 2, then goes back to one, even when we don't exit pry. Is that normal?
-
   puts "Sending response."
-  response = "<pre>
-Verb: POST
-Path: /
-Protocol: HTTP/1.1
-Host: 127.0.0.1
-Port: 9292
-Origin: 127.0.0.1
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-</pre>"
-  output = "<html><head></head><body> Hello, World! #{visited} \n #{response}</body></html>"
+  response = "<pre>" + request_lines.join("\n") + "</pre>"
+  output = "<html><head></head><body> Hello, World! (#{visited}) \n #{response}</body></html>"
   headers = ["http/1.1 200 ok",
              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
              "server: ruby",
@@ -34,7 +24,6 @@ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0
   client.puts headers
   client.puts output
   puts ["Wrote this response:", headers, output].join("\n")
+  client.close
 end
-client.close
-
 puts "\nRepsonse complete, exiting."
